134fa3878f07e2e92debe30acaa4fb01b98500de
Your Name <renee.petit@gmail.com>
2021-09-14 15:10:10 -0700

    movie selector works

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const seats = document.querySelectorAll('.row .seat:not(.occupied)');
 const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
+
+populateUI()
+
 let ticketPrice = +movieSelect.value;
 
 // Save selected movie index and price
@@ -24,6 +27,25 @@ function updateSelectedCount() {
     total.innerText = selectedSeatsCount * ticketPrice;
 }
 
+// get data from local storage and populate UI
+function populateUI() {
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+
+    if(selectedSeats !== null && selectedSeats.length > 0) {
+        seats.forEach((seat, index) => {
+            if(selectedSeats.indexOf(index) > -1) {
+                seat.classList.add('selected');
+            }
+        });
+    }
+
+    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+
+    if(selectedMovieIndex !== null) {
+        movieSelect.selectedIndex = selectedMovieIndex;
+    }
+}
+
 // Movie select event
 movieSelect.addEventListener('change', (e) => {
     ticketPrice = +e.target.value;
@@ -39,4 +61,7 @@ container.addEventListener('click', (e) => {
         e.target.classList.toggle('selected');
         updateSelectedCount();
     }
-})
+});
+
+// initial count and total set
+updateSelectedCount();
